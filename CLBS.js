3536f876f810e28bf344e1ff4efee31e64bbd674
bene-tan <125938524+bene-tan@users.noreply.github.com>
2023-02-21 11:50:33 +0800

    Update CLBS.js

--- CLBS.js
+++ CLBS.js
@@ -1,5 +1,5 @@
 function changeTextFormat() {
-var valueTwo = loader.engine.document.getElementById(0000000e).getProperty('value.value');
-loader.engine.document.getElementById(0000000e).setValue(({"value": valueTwo.toUpperCase()})); 
+var valueTwo = loader.engine.document.getElementById(104009226).getProperty('value.value');
+loader.engine.document.getElementById(104009226).setValue(({"value": valueTwo.toUpperCase()})); 
 }
 window.onchange = changeTextFormat;
